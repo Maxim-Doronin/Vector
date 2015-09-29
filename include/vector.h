01b281d7a6750eb627f14be19e8b89a21a6822b8
Maxim Doronin <doro156@me.com>
2015-09-29 20:36:22 +0300

    class vector has created

---
+++ include/vector.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class vector {
+private:
+	int size;
+	float *vmas;
+public:
+	vector(int size);
+	vector(vector& v);
+	~vector();
+	vector operator+(const vector& f);
+	vector operator-(const vector& f);
+	int operator*(const vector &f);
+	int operator==(const vector& f) const;
+	int operator!=(const vector& f) const;
+	const vector& operator=(const vector& f);
+}
