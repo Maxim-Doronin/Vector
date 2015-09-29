2c9c2cf575737c3f75b1d2aef856c416e27b039b
Maxim Doronin <doro156@me.com>
2015-09-29 22:02:33 +0300

    add get-methods

--- include/vector.h
+++ include/vector.h
@@ -3,7 +3,7 @@
 class vector {
 private:
 	int size;
-	float *vmas;
+	float *mas;
 public:
 	vector(int size);
 	vector(vector& v);
@@ -11,9 +11,9 @@ public:
 	vector operator+(const vector& f);
 	vector operator-(const vector& f);
 	int operator*(const vector &f);
-	int operator==(const vector& f) const;
-	int operator!=(const vector& f) const;
+	bool operator==(const vector& f) const;
+	bool operator!=(const vector& f) const;
 	const vector& operator=(const vector& f);
+	int get_size() {return size;};
+	float* ger_mas() {return mas;};
 }
-
-//test
